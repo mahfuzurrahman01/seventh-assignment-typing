6d650553b90f6d90ef2d2b073474cc28e796b9e0
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-09-15 02:38:39 +0600

    mobile tablet device responsive done

--- history.js
+++ history.js
@@ -15,7 +15,7 @@ function addHistory(questionText, timeTaken, errorCount) {
   histories.appendChild(newRow);
 
   let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
-  previousTests.unshift({ questionText, timeTaken, errorCount });
+  previousTests.push({ questionText, timeTaken, errorCount });
   localStorage.setItem("testHistory", JSON.stringify(previousTests));
 
   displayHistory();
